d179793a7c64ded98f67425bf31b1937200b9c41
mohammed Elsdody <31186483+mohamedma872@users.noreply.github.com>
2021-09-05 15:23:30 +0200

    add Inactive case to Taxi model , Base View Model

--- Coding-Challenge/Coding-Challenge/Modules/TaxiList/ViewModel/TaxiViewModel.swift
+++ Coding-Challenge/Coding-Challenge/Modules/TaxiList/ViewModel/TaxiViewModel.swift
@@ -6,8 +6,53 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
 enum TaxiTableViewCellType {
     case normal(cellViewModel: TaxiCellViewModel)
     case error(message: String)
     case empty
+}
+class TaxiViewModel : BaseViewModel {
+    
+    let disposeBag = DisposeBag()
+    private let networkInstance: TaxiNetworkProtocal
+    init(networkInstance : TaxiNetworkProtocal = TaxiNetworkManager()) {
+        self.networkInstance = networkInstance
+        CellsObservable = cells.asObservable()
+    }
+    private let loadingBehavior = BehaviorRelay<Bool>(value: false)
+    var onShowLoading: Observable<Bool> {
+            return loadingBehavior
+                .asObservable()
+                .distinctUntilChanged()
+        }
+  
+   private let cells = BehaviorRelay<[TaxiTableViewCellType]>(value: [])
+    var CellsObservable: Observable<[TaxiTableViewCellType]>
+
+    let onShowError = PublishSubject<String>()
+    func getTaxi(northLatitude: Double,eastLongitude
+                    : Double,southLatitude: Double,westLongitude: Double) {
+        loadingBehavior.accept(true)
+        networkInstance
+            .getTaxi(northLatitude: northLatitude, eastLongitude: eastLongitude, southLatitude: southLatitude, westLongitude: westLongitude)
+            .subscribe(
+                onNext: { [weak self] taxiModel in
+                    self?.loadingBehavior.accept(false)
+                    guard taxiModel?.poiList?.count ?? 0 > 0 else {
+                        self?.cells.accept([.empty])
+                        return
+                    }
+
+                    self?.cells.accept((taxiModel?.poiList?.compactMap { .normal(cellViewModel: TaxiCellViewModel(model: $0 )) })!)
+                },
+                onError: { [weak self] error in
+                    self?.loadingBehavior.accept(false)
+                    self?.onShowError.onNext(error.localizedDescription)
+                }
+            )
+            .disposed(by: disposeBag)
+     }
+   
 }
